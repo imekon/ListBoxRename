d097a28eb51f5070842fd305aefc5c2fd33ab0a1
Pete Goodwin <imekon@users.noreply.github.com>
2021-05-17 15:48:57 +0100

    Initial commit

---
+++ ItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace ListBoxRename
+{
+    public class ItemViewModel : ViewModelBase
+    {
+        private string m_name;
+
+        public ItemViewModel(string name)
+        {
+            m_name = name;
+        }
+
+        public string Name
+        {
+            get => m_name;
+            set
+            {
+                m_name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Rename(string newName)
+        {
+            m_name = newName;
+            OnPropertyChanged(nameof(Name));
+        }
+
+        public override string ToString()
+        {
+            return m_name;
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace ListBoxRename
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<ItemViewModel> m_items;
+        private ItemViewModel m_selected;
+
+        public MainWindowViewModel()
+        {
+            m_items = new ObservableCollection<ItemViewModel>();
+
+            m_items.Add(new ItemViewModel("First"));
+            m_items.Add(new ItemViewModel("Second"));
+            m_items.Add(new ItemViewModel("Third"));
+
+            m_selected = m_items[1];
+        }
+
+        public ObservableCollection<ItemViewModel> Items => m_items;
+
+        public ItemViewModel Selected
+        {
+            get => m_selected;
+            set
+            {
+                m_selected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand RenameCommand
+        {
+            get
+            {
+                return new DelegateCommand(o =>
+                {
+                    m_items[1].Rename("Second Renamed");
+                });
+            }
+        }
+
+        public ICommand UnrenameCommand
+        {
+            get
+            {
+                return new DelegateCommand(o =>
+                {
+                    m_items[1].Rename("Second");
+                });
+            }
+        }
+
+        public ICommand ListCommand
+        {
+            get
+            {
+                return new DelegateCommand(o =>
+                {
+                    foreach(var item in m_items)
+                    {
+                        Trace.WriteLine(item.Name);
+                    }
+                });
+            }
+        }
+    }
+}
